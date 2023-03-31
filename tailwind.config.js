a2bcf54851c0dbe80d683609f40b4c05e429e260
m2lovric <m2lovric@gmail.com>
2023-04-01 00:13:16 +0200

    Extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      primary: '#F9BC6E',
-    },
     extend: {
+      colors: {
+        primary: '#F9BC6E',
+      },
       fontFamily: {
         HG: ['Hanken Grotesk', 'sans-serif'],
       },
