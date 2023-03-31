a29ee9b6f2824ab31013003a1d1447523ae607a2
m2lovric <m2lovric@gmail.com>
2023-04-01 00:12:44 +0200

    Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import Layout from './components/Layout';
+import Button from './components/Button';
+import Hero from './assets/Hero.png';
 
 function App() {
   return (
     <Layout>
-      <h1>app</h1>
+      <section className='flex justify-between'>
+        <article className='mt-52 ml-64'>
+          <h1 className='text-7xl font-bold font-sans'>
+            We help you grow
+            <br />
+            your <span className='text-primary'>conversions.</span>
+          </h1>
+          <p className='text-2xl font-HG leading-10 my-6'>
+            Featured in leading publications around the world
+          </p>
+          <Button text='Contact Us' />
+        </article>
+        <img src={Hero} alt='hero' className='mt-16' />
+      </section>
     </Layout>
   );
 }
