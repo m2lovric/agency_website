797bbc3fa9da8f0916b9ee996454c92a3c18aa8a
m2lovric <m2lovric@gmail.com>
2023-04-02 00:03:05 +0200

    Paths to images

---
+++ src/images.ts
@@ -0,0 +1,7 @@
+import Competitors from './assets/howItWorks/Competitors.png';
+import Goal from './assets/howItWorks/Goal.png';
+import Launch from './assets/howItWorks/Launch.png';
+import Strategy from './assets/howItWorks/Strategy.png';
+import Hero from './assets/Hero.png';
+
+export { Competitors, Goal, Launch, Strategy, Hero };
