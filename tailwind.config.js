5f2cb5a1cbcecc282f876a78af4a3c3586fc52ca
m2lovric <m2lovric@gmail.com>
2023-04-02 00:01:52 +0200

    Update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,18 @@ module.exports = {
     extend: {
       colors: {
         primary: '#F9BC6E',
+        'soft-brown': '#C59E6F',
       },
       fontFamily: {
         HG: ['Hanken Grotesk', 'sans-serif'],
+      },
+      backgroundImage: {
+        'hero-pattern': "url('./src/assets/Wave.svg')",
+      },
+      container: {
+        padding: {
+          '2xl': '4rem',
+        },
       },
     },
   },
