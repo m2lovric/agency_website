7ddf7c9d44b90366372f8d55c58ab546b8dc337f
m2lovric <m2lovric@gmail.com>
2023-04-02 00:25:04 +0200

    Add images

--- src/images.ts
+++ src/images.ts
@@ -3,5 +3,19 @@ import Goal from './assets/howItWorks/Goal.png';
 import Launch from './assets/howItWorks/Launch.png';
 import Strategy from './assets/howItWorks/Strategy.png';
 import Hero from './assets/Hero.png';
+import Facebook from './assets/Facebook.png';
+import Instagram from './assets/Instagram.png';
+import LinkedIn from './assets/LinkedIn.png';
+import Twitter from './assets/Twitter.png';
 
-export { Competitors, Goal, Launch, Strategy, Hero };
+export {
+  Competitors,
+  Goal,
+  Launch,
+  Strategy,
+  Hero,
+  Facebook,
+  Instagram,
+  LinkedIn,
+  Twitter,
+};
