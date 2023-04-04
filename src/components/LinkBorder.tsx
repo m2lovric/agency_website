863852264d26a40de04899a86c2863e7daa68d73
m2lovric <m2lovric@gmail.com>
2023-04-04 20:17:06 +0200

    Add animation to Nav

---
+++ src/components/LinkBorder.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LinkBorder = () => {
+  return (
+    <div className='absolute -bottom-1 left-0 w-0 h-1 bg-primary transition-all duration-300 group-hover:w-full'></div>
+  );
+};
+
+export default LinkBorder;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import LinkBorder from './LinkBorder';
 
 const Nav = () => {
   return (
@@ -8,16 +9,32 @@ const Nav = () => {
           LOGO
         </Link>
         <section className='flex items-center justify-between w-1/2 text-lg font-semibold font-HG'>
-          <Link to={'/services'}>Services</Link>
-          <Link to={'/industries'}>Industries</Link>
-          <Link to={'/about'}>About Us</Link>
-          <Link to={'/team'}>Team Agency</Link>
-          <Link to={'/blog'}>Blog</Link>
+          <Link to={'/services'} className='relative group'>
+            Services
+            <LinkBorder />
+          </Link>
+          <Link to={'/industries'} className='relative group'>
+            Industries
+            <LinkBorder />
+          </Link>
+          <Link to={'/about'} className='relative group'>
+            About Us
+            <LinkBorder />
+          </Link>
+          <Link to={'/team'} className='relative group'>
+            Team Agency
+            <LinkBorder />
+          </Link>
+          <Link to={'/blog'} className='relative group'>
+            Blog
+            <LinkBorder />
+          </Link>
           <Link
             to={'/contact'}
-            className='text-primary border border-primary py-2 px-6 rounded-full'
+            className='text-primary relative group border border-primary bg-transparent py-2 px-6 rounded-full hover:text-white'
           >
             Contact Us
+            <div className='bg-primary w-0 h-full opacity-0 absolute top-0 left-0 rounded-full -z-10 transition-all duration-300 group-hover:w-full group-hover:opacity-100'></div>
           </Link>
         </section>
       </section>
