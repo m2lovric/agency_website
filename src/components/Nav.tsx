048aab3e0b49b356eeb5f9785a0dd5f972678e0d
m2lovric <m2lovric@gmail.com>
2023-04-16 22:36:47 +0200

    Nav update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -39,53 +39,85 @@ const Nav = () => {
             />
           </button>
         )}
-        {window.window.outerWidth < 640 && clicked && (
-          <AnimatePresence initial={true}>
-            <motion.section
-              className='flex items-center justify-between z-10 w-1/2 text-lg font-semibold font-HG sm:absolute sm:top-28 sm:w-full sm:rounded-2xl sm:shadow-2xl sm:gap-5 sm:items-center sm:py-10 sm:bg-white sm:flex-col'
-              initial={{
-                y: -200,
-                display: 'none',
-              }}
-              animate={{
-                y: 20,
-                display: 'flex',
-              }}
-              transition={{ duration: 0.1, type: 'tween' }}
-              exit={{
-                y: -200,
-                display: 'none',
-              }}
-            >
-              <Link to={'/services'} className='relative group'>
-                Services
-                <LinkBorder />
-              </Link>
-              <Link to={'/industries'} className='relative group'>
-                Industries
-                <LinkBorder />
-              </Link>
-              <Link to={'/about'} className='relative group'>
-                About Us
-                <LinkBorder />
-              </Link>
-              <Link to={'/team'} className='relative group'>
-                Team Agency
-                <LinkBorder />
-              </Link>
-              <Link to={'/blog'} className='relative group'>
-                Blog
-                <LinkBorder />
-              </Link>
-              <Link
-                to={'/contact'}
-                className='text-primary relative group border border-primary bg-transparent py-2 px-6 rounded-full hover:text-white'
+        {window.window.outerWidth < 640 ? (
+          clicked && (
+            <AnimatePresence initial={true}>
+              <motion.section
+                className='flex items-center justify-between z-10 w-1/2 text-lg font-semibold font-HG sm:absolute sm:top-28 sm:w-full sm:rounded-2xl sm:shadow-2xl sm:gap-5 sm:items-center sm:py-10 sm:bg-white sm:flex-col'
+                initial={{
+                  y: -200,
+                  display: 'none',
+                }}
+                animate={{
+                  y: 20,
+                  display: 'flex',
+                }}
+                transition={{ duration: 0.3, type: 'tween' }}
+                exit={{
+                  y: -200,
+                  display: 'none',
+                }}
               >
-                Contact Us
-                <div className='bg-primary w-0 h-full opacity-0 absolute top-0 left-0 rounded-full -z-10 transition-all ease-in-out duration-300 group-hover:w-full group-hover:opacity-100'></div>
-              </Link>
-            </motion.section>
-          </AnimatePresence>
+                <Link to={'/services'} className='relative group'>
+                  Services
+                  <LinkBorder />
+                </Link>
+                <Link to={'/industries'} className='relative group'>
+                  Industries
+                  <LinkBorder />
+                </Link>
+                <Link to={'/about'} className='relative group'>
+                  About Us
+                  <LinkBorder />
+                </Link>
+                <Link to={'/team'} className='relative group'>
+                  Team Agency
+                  <LinkBorder />
+                </Link>
+                <Link to={'/blog'} className='relative group'>
+                  Blog
+                  <LinkBorder />
+                </Link>
+                <Link
+                  to={'/contact'}
+                  className='text-primary relative group border border-primary bg-transparent py-2 px-6 rounded-full hover:text-white'
+                >
+                  Contact Us
+                  <div className='bg-primary w-0 h-full opacity-0 absolute top-0 left-0 rounded-full -z-10 transition-all ease-in-out duration-300 group-hover:w-full group-hover:opacity-100'></div>
+                </Link>
+              </motion.section>
+            </AnimatePresence>
+          )
+        ) : (
+          <section className='flex items-center justify-between z-10 w-1/2 text-lg font-semibold font-HG sm:absolute sm:top-28 sm:w-full sm:rounded-2xl sm:shadow-2xl sm:gap-5 sm:items-center sm:py-10 sm:bg-white sm:flex-col'>
+            <Link to={'/services'} className='relative group'>
+              Services
+              <LinkBorder />
+            </Link>
+            <Link to={'/industries'} className='relative group'>
+              Industries
+              <LinkBorder />
+            </Link>
+            <Link to={'/about'} className='relative group'>
+              About Us
+              <LinkBorder />
+            </Link>
+            <Link to={'/team'} className='relative group'>
+              Team Agency
+              <LinkBorder />
+            </Link>
+            <Link to={'/blog'} className='relative group'>
+              Blog
+              <LinkBorder />
+            </Link>
+            <Link
+              to={'/contact'}
+              className='text-primary relative group border border-primary bg-transparent py-2 px-6 rounded-full hover:text-white'
+            >
+              Contact Us
+              <div className='bg-primary w-0 h-full opacity-0 absolute top-0 left-0 rounded-full -z-10 transition-all ease-in-out duration-300 group-hover:w-full group-hover:opacity-100'></div>
+            </Link>
+          </section>
         )}
       </section>
     </nav>
