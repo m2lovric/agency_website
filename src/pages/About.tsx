b5a034b277993e1fc89a388d32c2307857fce31e
m2lovric <m2lovric@gmail.com>
2023-04-19 19:57:51 +0200

    About page update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,35 +16,56 @@ const About = () => {
         button={false}
       />
 
-      <section className='container mx-auto flex flex-col'>
+      <section className='container mx-auto flex flex-col sm:px-10'>
         <h1 className='mb-14 mt-72 text-center font-sans text-5xl font-bold'>
           Make a Difference For Your Business!
         </h1>
-        <section className='flex justify-between'>
-          <p>
+        <section className='flex items-center justify-between sm:flex-col-reverse'>
+          <p className='w-1/2 font-HG text-xl leading-8 sm:w-full'>
             We understand that not every company has an in-house department that
             is always updated and tracks the algorithms, trends, and changes
             within digital marketing, but we strongly believe every company
-            should have somebody to do so! Therefore, we started Lunar Strategy
-            digital marketing agency – to offer companies solutions that are
-            completely customized to fit their business needs and goals. We
-            tailor high-performing marketing strategies intended to serve and
-            benefit your company long term. Lunar Strategy is a professional
-            digital marketing agency with a highly motivated team.
+            should have somebody to do so!
+            <br />
+            <br />
+            Therefore, we started Lunar Strategy digital marketing agency – to
+            offer companies solutions that are completely customized to fit
+            their business needs and goals.
+            <br />
+            <br />
+            We tailor high-performing marketing strategies intended to serve and
+            benefit your company long term.
+            <br />
+            <br />
+            Lunar Strategy is a professional digital marketing agency with a
+            highly motivated team.
           </p>
-          <img src={AboutSolution} alt='solution' />
+          <img
+            src={AboutSolution}
+            alt='solution'
+            className='w-1/2 sm:w-full sm:py-10'
+          />
         </section>
-        <section className='flex justify-between'>
-          <img src={AboutProduct} alt='product' />
-          <p>
+        <section className='mt-20 flex items-center justify-between sm:flex-col'>
+          <img
+            src={AboutProduct}
+            alt='product'
+            className='w-1/2 sm:w-full sm:py-10'
+          />
+          <p className='w-1/2 font-HG text-xl leading-8 sm:w-full'>
             Most of us have profound hands-on experience working with and for
             Google and Google Ads directly. Our marketing masters also have
-            experience with Facebook Ads and SEO. Our team lives and breathes
-            innovative, quick, and smart digital marketing solutions. We want to
-            help as many businesses as possible to find new potential customers,
-            reengage with old ones to reach far beyond growth expectations. Book
-            a meeting with us today, free of charge, and we’ll talk about how we
-            can implement a thorough and solid marketing strategy.
+            experience with Facebook Ads and SEO.
+            <br />
+            <br />
+            Our team lives and breathes innovative, quick, and smart digital
+            marketing solutions. We want to help as many businesses as possible
+            to find new potential customers, reengage with old ones to reach far
+            beyond growth expectations.
+            <br />
+            <br />
+            Book a meeting with us today, free of charge, and we’ll talk about
+            how we can implement a thorough and solid marketing strategy.
           </p>
         </section>
       </section>
